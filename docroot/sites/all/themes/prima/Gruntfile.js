b9e465007f1bde592a6e164bdda042c3739f5669
marapredescu1 <mara.predescu@eaudeweb.ro>
2017-02-07 17:11:06 +0200

    Added postcss autoprefixer

--- docroot/sites/all/themes/prima/Gruntfile.js
+++ docroot/sites/all/themes/prima/Gruntfile.js
@@ -1,20 +1,37 @@
 module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
 
   grunt.initConfig({
     watch: {
       files: "less/*.less",
-      tasks: ["less"]
+      tasks: ["less", "postcss"]
     },
     less: {
       development: {
         options: {
-          paths: ["less/"]
+          paths: ["less/"],
+          outputSourceFiles: true,
+          sourceMap: true,
+          sourceMapFilename: 'css/style.css.map',
+          sourceMapURL: 'style.css.map',
+          strictMath: true,
         },
         files: {
           "css/style.css": "less/style.less"
         }
+      }
+    },
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')
+        ]
+      },
+      core: {
+        src: 'css/*.css'
       }
     }
   });
